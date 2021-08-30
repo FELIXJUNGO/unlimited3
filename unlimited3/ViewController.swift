975e888734208c409b57cec1aabbc08eacfce471
FJUHWZ <42535257+FJUHWZ@users.noreply.github.com>
2021-08-30 16:40:12 +0200

    mac pro

--- unlimited3/ViewController.swift
+++ unlimited3/ViewController.swift
@@ -27,33 +27,68 @@ let locationManager = CLLocationManager()
             locationManager.startUpdatingLocation()
         }
         
+    }
+    
+    private lazy var placeAsIf5: Void = {
+            print("place model 1")
+            // Load the "Box" scene from the "Experience" Reality File
+            let asif5Anchor = try! Experience.loadAsIf5()
+            // Add the box anchor to the scene
+            arView.scene.anchors.append(asif5Anchor)
+    }()
+    
+    private lazy var placeBox: Void = {
+            print("place Box")
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
-    }
+    }()
+    
+    
+
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             
             //print(location.coordinate)
-            let coordinate1 = CLLocation(latitude: 47.407862, longitude: 8.550523)
-            let coordinateNow = location
-            let distanceInMeters = coordinate1.distance(from: coordinateNow)
-            print(distanceInMeters)
-            if(distanceInMeters <= 15)
-             {
-             print("less than 15m")
-                // Load the "Box" scene from the "Experience" Reality File
-                let asif5Anchor = try! Experience.loadAsIf5()
-                // Add the box anchor to the scene
-                arView.scene.anchors.append(asif5Anchor)
-             }
-             else
-            {
-             // out of 1 mile
-             }
+            let coordinatesAsIf5 = CLLocation(latitude: 47.407862, longitude: 8.550523)
+            let coordinatesBox = CLLocation(latitude: 47.407862, longitude: 8.550523)
+            let coordinate3 = CLLocation(latitude: 47.407862, longitude: 8.550523)
+            let coordinate4 = CLLocation(latitude: 47.407862, longitude: 8.550523)
+            let coordinate5 = CLLocation(latitude: 47.407862, longitude: 8.550523)
             
-            print(distanceInMeters)
+            let coordinateNow = location
+            let distanceToAsIf5 = coordinatesAsIf5.distance(from: coordinateNow)
+            let distanceToModel2 = coordinatesBox.distance(from: coordinateNow)
+            let distanceToModel3 = coordinate3.distance(from: coordinateNow)
+            let distanceToModel4 = coordinate4.distance(from: coordinateNow)
+            let distanceToModel5 = coordinate5.distance(from: coordinateNow)
+            
+
+            
+            
+            print(distanceToAsIf5)
+            if(distanceToAsIf5 <= 10)
+             {
+                _ = placeAsIf5
+             }
+            if(distanceToModel2 <= 10)
+             {
+                _ = placeBox
+             }
+            if(distanceToModel3 <= 10)
+             {
+                _ = placeAsIf5
+             }
+            if(distanceToModel4 <= 10)
+             {
+                _ = placeAsIf5
+             }
+            if(distanceToModel5 <= 10)
+             {
+                _ = placeAsIf5
+             }
             
         }
     }
