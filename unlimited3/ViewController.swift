b201acfeef5c0e69495e7d6cca1578ac65a41150
FJUHWZ <42535257+FJUHWZ@users.noreply.github.com>
2021-08-30 15:50:49 +0200

    mac studio

--- unlimited3/ViewController.swift
+++ unlimited3/ViewController.swift
@@ -29,13 +29,32 @@ let locationManager = CLLocationManager()
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            print(location.coordinate)
+            
+            //print(location.coordinate)
+            let coordinate1 = CLLocation(latitude: 47.407862, longitude: 8.550523)
+            let coordinateNow = location
+            let distanceInMeters = coordinate1.distance(from: coordinateNow)
+            print(distanceInMeters)
+            if(distanceInMeters <= 15)
+             {
+             print("less than 15m")
+                // Load the "Box" scene from the "Experience" Reality File
+                let asif5Anchor = try! Experience.loadAsIf5()
+                // Add the box anchor to the scene
+                arView.scene.anchors.append(asif5Anchor)
+             }
+             else
+            {
+             // out of 1 mile
+             }
+            
+            print(distanceInMeters)
+            
         }
     }
 }
